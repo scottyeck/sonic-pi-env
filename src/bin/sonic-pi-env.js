47e2a0bd87826db8f6798a2e2f254e5e43819c4c
Scotty Eckenthal <scott.eckenthal@gmail.com>
2017-12-24 16:53:41 -0500

    Prettier / formatting

--- src/bin/sonic-pi-env.js
+++ src/bin/sonic-pi-env.js
@@ -9,18 +9,16 @@ import program from 'commander';
 const guiId = '';
 
 const getFilename = filename => {
-    return path.join(process.cwd(), filename);
+  return path.join(process.cwd(), filename);
 };
 
-program
-    .command('play')
-    .action(function() {
-        const [filename] = this.parent.args;
-        const file = fs.readFileSync(getFilename(filename), 'utf-8');
-        const client = new osc.Client('127.0.0.1', 4557);
-        client.send('/run-code', guiId, file, () => {
-            client.kill();
-        });
-    });
+program.command('play').action(function() {
+  const [filename] = this.parent.args;
+  const file = fs.readFileSync(getFilename(filename), 'utf-8');
+  const client = new osc.Client('127.0.0.1', 4557);
+  client.send('/run-code', guiId, file, () => {
+    client.kill();
+  });
+});
 
 program.parse(process.argv);
